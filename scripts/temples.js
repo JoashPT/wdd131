574a732a68f6bf7dd9ac48301a71e8f54dc11117
JoashPT <joashtantiangco@yahoo.com>
2024-05-05 00:16:47 +0800

    temple week 02 requirement

---
+++ scripts/temples.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('show');
+    navigation.classList.toggle('show');
+})
+
+// FOR FOOTER
+// select the DOM element for output
+const year = document.querySelector("#currentYear");
+const lstModif = document.querySelector('#lastModified');
+
+// use the date object
+const today = new Date();
+
+year.innerHTML = `<span id="currentyear">${today.getFullYear()}</span>`;
+lstModif.innerHTML = new Date(document.lastModified);
+
